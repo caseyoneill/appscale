ea1fcb2d9494926c860f4f384104861ca18b6749
Casey O'Neill <j.casey.oneill@gmail.com>
2015-06-03 12:45:54 -0600

    Checking in changes that finally have the server working

--- AppServer_Java/src/com/google/appengine/api/blobstore/dev/BlobStorageFactory.java
+++ AppServer_Java/src/com/google/appengine/api/blobstore/dev/BlobStorageFactory.java
@@ -2,12 +2,15 @@ package com.google.appengine.api.blobstore.dev;
 
 
 import java.io.File;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import com.google.appengine.api.blobstore.dev.BlobInfoStorage;
 import com.google.appengine.api.blobstore.dev.BlobStorage;
 
 public class BlobStorageFactory
 {
+    private static final Logger		 logger = Logger.getLogger(BlobStorageFactory.class.getName());
     private static final BlobInfoStorage blobInfoStorage = new BlobInfoStorage();
     private static BlobStorage           blobStorage;
 
@@ -18,10 +21,13 @@ public class BlobStorageFactory
 
     public static BlobStorage getBlobStorage()
     {
+	//logger.fine("Retrieving BlobStorage..");
         if (blobStorage == null)
         {
+            logger.severe("BlobStorage is null.");
             throw new IllegalStateException("Must call one of set*BlobStorage() first.");
         }
+	//logger.fine("BlobStorage retrieved.");
         return blobStorage;
     }
 
@@ -38,5 +44,6 @@ public class BlobStorageFactory
     public static void setDatastoreBlobStorage()
     {
         blobStorage = new DatastoreBlobStorage(blobInfoStorage);
+        //logger.fine("BlobStorage created.");
     }
 }
